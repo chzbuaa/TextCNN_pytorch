e510b184a433cc9114ef2ef2136d8e372fb42d99
chzbuaa <18713596689@163.com>
2020-11-01 20:00:26 +0800

    first commit

---
+++ get_my_trainData.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+'''
+从原数据中选取部分数据；
+选取数据的title前两个字符在字典WantedClass中；
+且各个类别的数量为WantedNum
+'''
+import jieba
+import json
+
+TrainJsonFile = 'baike_qa_valid.json'
+MyTainJsonFile = 'my_validdata.json'
+StopWordFile = 'stopword.txt'
+
+WantedClass = {'教育': 0, '健康': 0, '生活': 0, '娱乐': 0, '游戏': 0}
+WantedNum = 5000
+numWantedAll = WantedNum * 5
+
+
+def main():
+    Datas = open(TrainJsonFile, 'r', encoding='utf_8').readlines()
+    f = open(MyTainJsonFile, 'w', encoding='utf_8')
+
+    numInWanted = 0
+    for line in Datas:
+        data = json.loads(line)
+        cla = data['category'][0:2]
+        print(cla)
+        if cla in WantedClass and WantedClass[cla] < WantedNum:
+            json_data = json.dumps(data, ensure_ascii=False)
+            f.write(json_data)
+            f.write('\n')
+            WantedClass[cla] += 1
+            numInWanted += 1
+            if numInWanted >= numWantedAll:
+                break
+
+
+if __name__ == '__main__':
+    main()
